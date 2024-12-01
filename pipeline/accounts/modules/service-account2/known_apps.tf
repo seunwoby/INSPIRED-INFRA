cdae52a75da9377ce4e63c3ce763ac07c43f17ad
Oluseun Rotimi Okuwobi <seunwoby@hotmail.com>
2024-12-01 16:49:42 +0000

    redo

--- pipeline/accounts/modules/service-account2/known_apps.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azuread_application_pre_authorized" "preauth" {
-  count = length(var.known_apps)
-
-  application_id       = azuread_application.app.id
-  authorized_client_id = var.known_apps[count.index].client_id
-
-  permission_ids = [ for p in var.known_apps[count.index].scopes : azuread_application.app.oauth2_permission_scope_ids[p] ]
-}
