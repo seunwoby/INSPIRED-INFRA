1463e9363fa979d4123bd1032e2b91cdeae87bc3
Oluseun Rotimi Okuwobi <seunwoby@hotmail.com>
2024-12-01 15:48:50 +0000

    first Config

---
+++ pipeline/terraform-manifests/accounts/uat/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.50.0"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name = "Terraform"
+    storage_account_name = "adminiegterraform"
+    container_name = "accounts"
+    key = "uat.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features { }
+}
+
